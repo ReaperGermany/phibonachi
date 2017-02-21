ccb59f6a4ec4aee64f95d1048a8532d2823da5a6
TK <TK@Mac-mini-frontend.local>
2017-02-21 13:30:05 +0300

    Scala

---
+++ app/controllers/PhibController.scala
@@ -0,0 +1,35 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import scala.collection.immutable.ListMap
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class PhibController extends Controller {
+
+  def nthFibonacci(i: Int): Int = {
+    if (i == 1 || i == 2)
+      return i
+    return nthFibonacci(i - 1) + nthFibonacci(i - 2)
+  }
+
+  def index(limit: Int) = Action {
+    _ => {
+      var mas : Array[Int] = new Array[Int](limit)
+      val map = scala.collection.mutable.Map[String,String]()
+      mas(0) = 1;
+      for (i <- 1 to (mas.length - 1)){
+          mas(i) = nthFibonacci(i)
+          val num = nthFibonacci(i)
+          map += "Element " + i.toString -> num.toString
+      }
+
+      Ok(" " + ListMap(map.toSeq.sortWith(_._1 < _._1):_*))
+    }
+  }
+}
